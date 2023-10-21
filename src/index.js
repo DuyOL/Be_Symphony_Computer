9a2850d1cee0d4e7d080c5e87dc0f5d2de8b01f0
DuyOL <duyskt228@gmail.com>
2023-10-21 18:26:20 +0700

    20-10

--- src/index.js
+++ src/index.js
@@ -1,24 +1,24 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const  mongoose  = require('mongoose');
+const routes = require('./routers');
+const bodyParser = require('body-parser');
 dotenv.config()
 
 const app = express();
 const port = process.env.PORT || 3001;
+app.use(bodyParser.json())
 
-app.get('/', (req, res) => {
-    return res.send('Hello World');
-})
-console.log('process.env.USER_MOGODB', process.env.USER_MOGODB)
-console.log('process.env.MONGO_DB', process.env.MONGO_DB)
+routes(app)
 
-mongoose.connect(`mongodb+srv://James:FyQNgrJvMhkMR3A1@cluster0.iwyvqx3.mongodb.net/?retryWrites=true&w=majority`)
+mongoose.connect(`${process.env.MONGO_DB}`)
 .then(() => {
     console.log('Connected to MongoDB success');
 })
 .catch((err) => {
    console.log(err);   
 })
-app.listen(dotenv.PORT ,() => {
-    console.log(`Server running on port `, + dotenv.PORT);
+
+app.listen(port ,() => {
+    console.log(`Server running on port `, + port);
 })
