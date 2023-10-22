793385446b2f1aede2227cb65d841c5d09dbbd61
DuyOL <duyskt228@gmail.com>
2023-10-23 00:50:50 +0700

    23-10 kiem tra address email

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -1,17 +1,40 @@
 const UserService = require('../services/UserService')
 
 const createUser = async (req, res) => {
-    try{
-        console.log(req.body)
-        const res = await UserService.createUser()
-        return res.status(200).json(res)
-    }
-    catch(e){
+    try {
+        const { name, email, password, confirmPassword, phone } = req.body;
+        // hàm regedit check email 
+        const reg = /^\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$/
+        const isCheckEmail = reg.test(email);
+        if (!name || !email || !password || !confirmPassword || !phone) {
+            return res.status(200).json({
+                status: 'ERR',
+                message: 'The input is required'
+            });
+            // Check có phải email không
+        } else if (!isCheckEmail) {
+            return res.status(200).json({
+                status: 'ERR',
+                message: 'The input is email'
+            });
+            // Check password có trùng với confirmPassword không
+        } else if (password !== confirmPassword) {
+            return res.status(200).json({
+                status: 'ERR',
+                message: 'The password is not equal confirmPassword'
+            });
+        }
+        
+        console.log('isCheckEmail', isCheckEmail);
+        const response = await UserService.createUser(req.body);
+        return res.status(200).json(response);
+    } catch (e) {
         return res.status(404).json({
             message: e
-        })
+        });
     }
 }
+
 module.exports = {
     createUser
 }
