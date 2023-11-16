96abadaa77454a89ba9f0b25eb93e16bad3e1eb2
DuyOL <duyskt228@gmail.com>
2023-11-16 17:23:48 +0700

    16-11 API CRUD cho Product

--- src/middleware/authMiddleware.js
+++ src/middleware/authMiddleware.js
@@ -2,27 +2,38 @@ const jwt = require('jsonwebtoken')
 const dotenv = require('dotenv')
 dotenv.config()
 
-const authMiddleware = (req, res ,next) => {
-    console.log('checkToken' ,req.headers.token)
-    const token = req.headers.token.split(' ')[1]
-    jwt.verify(token, process.env.ACCESS_TOKEN, function(err, user) {
-        if(err){
+const authMiddleware = (req, res, next) => {
+    const tokenHeader = req.headers.token;
+
+    if (!tokenHeader) {
+        return res.status(404).json({
+            message: 'Token không được cung cấp',
+            status: 'ERROR',
+        });
+    }
+
+    const token = tokenHeader.split(' ')[1];
+
+    jwt.verify(token, process.env.ACCESS_TOKEN, function (err, user) {
+        if (err) {
             return res.status(404).json({
-                message: 'The authemtication',
-                status: 'ERROR'
-            })
+                message: 'Xác thực thất bại',
+                status: 'ERROR',
+            });
         }
-        const { payload } = user
-        if(payload?.isAdmin){
-            next()
+
+        const { payload } = user;
+        if (payload?.isAdmin) {
+            next();
         } else {
             return res.status(404).json({
-                message: 'The authemtication',
-                status: 'ERROR'
-            })
+                message: 'Xác thực thất bại',
+                status: 'ERROR',
+            });
         }
-      });
-}
+    });
+};
+
 const authUserMiddleware = (req, res ,next) => {
     console.log('checkToken' ,req.headers.token)
     const token = req.headers.token.split(' ')[1]
